3bdcdde6ac420cb3271c4857e137ff9c277b1216
Eric Brelsford <ebrelsford@gmail.com>
2014-04-30 13:45:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+Leaflet.snogylop
+================
+
+Leaflet.snogylop is a [Leaflet][] plugin that inverts polygons.
+
+Why? you ask. Sometimes you want to highlight a region of a map by
+de-emphasizing the rest of the map. Using this plugin you can set a relatively
+high opacity in order to de-emphasize everything outside of a polygon or
+multipolygon.
+
+
+## Demos
+
+An inverted [polygon][].
+
+An inverted [multipolygon][].
+
+
+## Usage
+
+Include `leaflet.snogylop.js` on your page, set the options on your GeoJSON 
+layer as specified below.
+
+Leaflet.snogylop is only tested on Leaflet version 0.7 or greater with L.GeoJSON
+layers. It may work with older Leaflets and should work with other layers. Pull
+requests in this direction are welcome.
+
+AMD compatible.
+
+
+### Options
+
+Define the following option in the vector options when creating a layer (eg, 
+an `L.GeoJSON`):
+
+ - **invert**: (boolean) True to invert. False by default.
+
+
+### Examples
+
+Create a GeoJSON layer, set `invert` to `true`:
+
+```javascript
+L.geoJson(data, {
+    invert: true
+}).addTo(map);
+```
+
+
+## License
+
+Leaflet.snogylop is free software, and may be redistributed under the MIT
+License.
+
+
+ [Leaflet]: https://github.com/Leaflet/Leaflet
---
+++ src/leaflet.snogylop.js
@@ -0,0 +1,68 @@
+(function () {
+
+    function defineSnogylop(L) {
+
+        var worldLatlngs = [
+            L.latLng([90, 180]),
+            L.latLng([90, -180]),
+            L.latLng([-90, -180]),
+            L.latLng([-90, 180])
+        ];
+
+        L.extend(L.Polygon.prototype, {
+
+            initialize: function (latlngs, options) {
+                if (options.invert && !options.invertMultiPolygon) {
+                    // Create a new set of latlngs, adding our world-sized ring
+                    // first
+                    var newLatlngs = [];
+                    newLatlngs.push(worldLatlngs);
+                    newLatlngs.push(latlngs[0]);
+                    latlngs = newLatlngs;
+                }
+
+                L.Polyline.prototype.initialize.call(this, latlngs, options);
+                this._initWithHoles(latlngs);
+			},
+
+        });
+
+        L.extend(L.MultiPolygon.prototype, {
+
+			initialize: function (latlngs, options) {
+				this._layers = {};
+				this._options = options;
+
+                if (options.invert) {
+                    // Let Polygon know we're part of a MultiPolygon
+                    options.invertMultiPolygon = true;
+
+                    // Create a new set of latlngs, adding our world-sized ring
+                    // first
+                    var newLatlngs = [];
+                    newLatlngs.push(worldLatlngs);
+                    for (var l in latlngs) {
+                        newLatlngs.push(latlngs[l][0]);
+                    }
+                    latlngs = [newLatlngs];
+                }
+
+				this.setLatLngs(latlngs);
+			},
+
+        });
+
+    }
+
+    if (typeof define === 'function' && define.amd) {
+        // Try to add snogylop to Leaflet using AMD
+        define(['leaflet'], function (L) {
+            defineSnogylop(L);
+        });
+    }
+    else {
+        // Else use the global L
+        defineSnogylop(L);
+    }
+
+})();
